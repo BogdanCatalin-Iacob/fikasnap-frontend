19118ed568ffb3035e791713a5c3cff5ed87983e
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-08-11 21:17:07 +0100

    Create NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const NavBar = () => {
+    return (
+        <Navbar expand="md" fixed="top">
+            <Container>
+                <Navbar.Brand>Logo placeholder</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default NavBar;
