476214921b7abcb3e3fca6f9b1f683cac1fd4cfc
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-08-14 19:08:49 +0100

    Update Navbar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css"
-import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom/cjs/react-router-dom";
 
 const NavBar = () => {
     return (
@@ -13,14 +13,14 @@ const NavBar = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
-                        <NavLink to="/">
-                            <i className="fas fa-home"></i>Home
+                        <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
+                            <i className="fas fa-home">Home</i>
                         </NavLink>
-                        <NavLink to="/signin">
-                            <i className="fas fa-sign-in-alt"></i>Sign In
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
+                            <i className="fas fa-sign-in">Sign in</i>
                         </NavLink>
-                        <NavLink to="/signup">
-                            <i className="fas fa-user-plus"></i>Sign Up
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
+                            <i className="fas fa-user-plus">Sign up</i>
                         </NavLink>
                     </Nav>
                 </Navbar.Collapse>
