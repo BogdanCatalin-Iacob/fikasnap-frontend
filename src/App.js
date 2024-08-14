2f2f788aa9d72c2146f0b99b65c504e47e2795bd
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-08-14 18:56:35 +0100

    Refactor: moved all routes in the Switch component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
-import { Route, Switch } from 'react-router-dom';
-
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom';
 function App() {
     return (
         <div className={styles.App}>
@@ -10,9 +9,8 @@ function App() {
             <Container className={styles.Main}>
                 <Switch>
                     <Route exact path="/" render={() => <h1>Home page</h1>} />
-                </Switch>
-                <Switch>
                     <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+                    <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
                 </Switch>
             </Container>
         </div>
