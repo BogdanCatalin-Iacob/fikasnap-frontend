a1310e6fc56895ff6c2551e51d67ee3f517a64eb
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-08-27 03:03:21 +0100

    Add a test backend route

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = 'https://fikasnap-app-d28b6f6fbe1b.herokuapp.com';
-axios.defaults.baseURL = 'https://django-rest-api-social-app-7dfc69d9cf56.herokuapp.com/';
+axios.defaults.baseURL = 'https://fikasnap-app-d28b6f6fbe1b.herokuapp.com/';
 // multipart header is required to send text and image
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
