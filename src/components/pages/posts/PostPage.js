6a5e2c00d73a13227edc3b6fe320caf4f93ed380
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-09-30 17:05:50 +0100

    Include Post component on page

--- src/components/pages/posts/PostPage.js
+++ src/components/pages/posts/PostPage.js
@@ -6,6 +6,7 @@ import Container from "react-bootstrap/Container";
 import appStyles from '../../../App.module.css';
 import { useParams } from "react-router-dom/cjs/react-router-dom";
 import { axiosRequest } from "../../../api/axiosDefaults";
+import Post from "./Post";
 
 function PostPage() {
     // fetch the param set in the Route
@@ -37,7 +38,8 @@ function PostPage() {
         <Row className="h-100">
             <Col className="py-2 p-0 p-lg-2" lg={8}>
                 <p>Popular profiles for mobile</p>
-                <p>Post component</p>
+                {/* pass post details */}
+                <Post {...post.results[0]} setPosts={setPost} postPage />
                 <Container className={appStyles.Content}>
                     Comments
                 </Container>
