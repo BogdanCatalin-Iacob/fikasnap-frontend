53ba1c2917925c870fc86ba4947011db1e7c129f
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-09-30 16:45:39 +0100

    update comment

--- src/components/Assets.js
+++ src/components/Assets.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Spinner } from "react-bootstrap";
 import styles from "../styles/Asset.module.css";
 
+/**Will render any combination of loader
+ * If a prop won't be passed the param will be ignored
+ */
 const Asset = ({ spinner, src, message }) => {
     return (
         <div className={`${styles.Asset} p-4`}>
