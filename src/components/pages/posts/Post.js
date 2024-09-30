71bbb135229df311dd9c29e57c8ae83f095121fc
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-09-30 17:33:20 +0100

    Add likes_count and comments_count to the post card depending on the user status

--- src/components/pages/posts/Post.js
+++ src/components/pages/posts/Post.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from '../../../styles/Post.module.css'
 import { useCurrentUser } from "../../../contexts/CurrentUserContext";
-import { Card, Media } from 'react-bootstrap';
+import { Card, Media, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import Avatar from "../../Avatar";
 
@@ -44,6 +44,33 @@ const Post = (props) => {
         <Card.Body>
             {title && <Card.Title className="text-center">{title}</Card.Title>}
             {content && <Card.Text>{content}</Card.Text>}
+            <div className={styles.PostBar}>
+                {is_owner ? (
+                    <OverlayTrigger placement="top" overlay={<Tooltip>You can't like your own post!</Tooltip>}>
+                        <i className="far fa-heart" />
+                    </OverlayTrigger>
+                ) : like_id ? (
+                    <span onClick={() => {}}>
+                        <i className={`fas fa-heart ${styles.Heart}`} />
+                    </span>
+                ) : currentUser ? (
+                    <span onClick={() => {}}>
+                        <i className={`far fa-heart ${styles.HeartOutline}`} />
+                    </span>
+                ) : (
+                    <OverlayTrigger
+                        placement="top"
+                        overlay={<Tooltip>Log in to like posts!</Tooltip>}
+                    >
+                        <i className="far fa-heart" />
+                    </OverlayTrigger>
+                )}
+                {likes_count}
+                <Link to={`/posts/${id}`}>
+                    <i className="far fa-comments" />
+                    {comments_count}
+                </Link>
+            </div>
         </Card.Body>
     </Card>
 }
