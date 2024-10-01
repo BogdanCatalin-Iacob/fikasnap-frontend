5bdad9c72e1bc9c149bafff0a24fc1c250d7e23f
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-10-01 18:39:20 +0100

    Add route to feeds and likes

--- src/App.js
+++ src/App.js
@@ -7,14 +7,22 @@ import SignUpForm from "./components/pages/auth/SignUpForm";
 import SignInForm from "./components/pages/auth/SignInForm";
 import PostCreateForm from "./components/pages/posts/PostCreateForm";
 import PostPage from "./components/pages/posts/PostPage";
+import PostsPage from "./components/pages/posts/PostsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+    const currentUser = useCurrentUser();
+    const profile_id = currentUser?.profile_id || "";
     return (
         <div className={styles.App}>
             <NavBar />
             <Container className={styles.Main}>
                 <Switch>
-                    <Route exact path="/" render={() => <h1>Home page</h1>} />
+                    <Route exact path="/" render={() => <PostsPage message="No results are found. Adjust search keyword!" />} />
+                    <Route exact path="/feed" render={() => <PostsPage message="No results are found. Adjust search keyword or follow a user!" />}
+                        filter={`owner__followed__owner__profile=${profile_id}&`} />
+                    <Route exact path="/liked" render={() => <PostsPage message="No results are found. Adjust search keyword or like a post!"
+                        filter={`likes__owner__profile=${profile_id}&ordering=-likes_created_at&`} />} />
                     <Route exact path="/signin" render={() => <SignInForm />} />
                     <Route exact path="/signup" render={() => <SignUpForm />} />
                     <Route exact path="/posts/create" render={() => <PostCreateForm />} />
