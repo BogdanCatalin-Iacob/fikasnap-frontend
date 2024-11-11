d11b8e4705df0bea87a2fcddc5a8031e8f7ceca1
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-11-11 19:41:00 +0000

    Add most followed profiles on modile devices

--- src/components/pages/profiles/PopularProfiles.js
+++ src/components/pages/profiles/PopularProfiles.js
@@ -11,7 +11,7 @@ import Asset from '../../Assets';
  * Utilizes internal modules for state management and API requests.
  * @returns {JSX.Element} A container component displaying the most followed profiles.
  */
-const PopularProfiles = () => {
+const PopularProfiles = ({ mobile }) => {
     const [profileData, setProfileData] = useState({
 
         // use the pageProfile later!
@@ -42,13 +42,22 @@ const PopularProfiles = () => {
     }, [currentUser]);
 
     return (
-        <Container className={appStyles.Content}>
+        <Container className={`${appStyles.Content} ${mobile && 'd-lg-none text-center mb-3'}`}>
             {popularProfiles.results.length ? (
                 <>
                     <p>Most followed profiles</p>
-                    {popularProfiles.results.map(profile => (
-                        <p key={profile.id}>{profile.owner}</p>
-                    ))}
+                    {/* display only 4 profiles on mobile devices */}
+                    {mobile ? (
+                        <div className='d-flex justify-content-around'>
+                            {popularProfiles.results.slice(0, 4).map(profile => (
+                                <p key={profile.id}>{profile.owner}</p>
+                            ))}
+                        </div>
+                    ) : (
+                        popularProfiles.results.map(profile => (
+                            <p key={profile.id}>{profile.owner}</p>
+                        ))
+                    )}
                 </>
             ) : (
                 <Asset spinner />
