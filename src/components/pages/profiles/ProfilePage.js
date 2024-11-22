41e0a3c0a6d59299850ebc5b0c60cf09a7d7b647
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2024-11-22 03:16:19 +0000

    Add profile dropdown on profile page for profile owner only

--- src/components/pages/profiles/ProfilePage.js
+++ src/components/pages/profiles/ProfilePage.js
@@ -21,6 +21,7 @@ import { Button, Image } from "react-bootstrap";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Post from "../posts/Post";
 import { fetchMoreData } from "../../../utils/utils";
+import { ProfileEditDropdown } from "../../MoreDropdown";
 
 /**
  * ProfilePage component displays a user's profile information and their posts.
@@ -66,6 +67,7 @@ function ProfilePage() {
 
     const mainProfile = (
         <>
+            {profile?.is_owner && <ProfileEditDropdown id={profile?.id} />}
             <Row noGutters className="px-3 text-center">
                 <Col lg={3} className="text-lg-left">
                     <Image className={styles.ProfileImage}
