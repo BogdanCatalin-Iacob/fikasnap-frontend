d064945fe66f1b4d115cc9bf22aab7bf55993966
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2024-12-19 20:05:41 +0000

    Add docstring

--- src/hooks/useClickOutsideToggle.js
+++ src/hooks/useClickOutsideToggle.js
@@ -1,5 +1,16 @@
 import { useEffect, useRef, useState } from "react";
 
+/**
+ * Custom hook that manages the toggle state of a burger menu and detects clicks outside of it.
+ * 
+ * @returns {Object} An object containing:
+ * - `expanded`: A boolean indicating whether the menu is expanded.
+ * - `setExpanded`: A function to update the expanded state.
+ * - `ref`: A ref object to be attached to the burger icon element.
+ * 
+ * The hook adds an event listener to the document to detect mouseup events
+ * and collapses the menu if a click occurs outside the referenced element.
+ */
 const useClickOutsideToggle = () => {
     // keep track of the burger menu
     const [expanded, setExpanded] = useState(false);
