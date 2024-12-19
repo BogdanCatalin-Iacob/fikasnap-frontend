dc699923a80d462af98f1ca165491f6cfe01027c
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2024-12-19 19:52:46 +0000

    Add docstring

--- src/components/pages/profiles/Profile.js
+++ src/components/pages/profiles/Profile.js
@@ -4,9 +4,23 @@ import btnStyles from '../../../styles/Button.module.css'
 import { useCurrentUser } from '../../../contexts/CurrentUserContext';
 import { Link } from 'react-router-dom';
 import Avatar from '../../Avatar';
-import { Button } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 import { useSetProfileData } from '../../../contexts/ProfileDataContext';
 
+/**
+ * Renders a user profile component with an avatar, username, and follow/unfollow button.
+ * 
+ * @param {Object} props - The properties passed to the component.
+ * @param {Object} props.profile - The profile data of the user.
+ * @param {boolean} props.mobile - Flag to determine if the component is in mobile view.
+ * @param {number} [props.imageSize=55] - The size of the avatar image.
+ * 
+ * @returns {JSX.Element} A JSX element representing the user profile.
+ * 
+ * The component displays the user's avatar and username. If the current user is not the profile owner,
+ * it shows a follow or unfollow button based on the following status. The layout adjusts based on the
+ * mobile flag.
+ */
 const Profile = (props) => {
     const { profile, mobile, imageSize = 55 } = props;
     const { id, following_id, image, owner } = profile;
