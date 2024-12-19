660834910dbda735be392b86c751062afd9a661c
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2024-12-19 20:03:51 +0000

    Add docstring

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,6 +3,12 @@ import NoResults from '../assets/no-results.png';
 import appStyles from '../styles/NotFound.module.css';
 import Asset from './Assets';
 
+/**
+ * NotFound component renders a message indicating that the requested page does not exist.
+ * It displays an image and a message using the Asset component.
+ *
+ * @returns {JSX.Element} A div containing the Asset component with a no-results image and a message.
+ */
 const NotFound = () => {
   return (
     <div className={appStyles.mt}>
