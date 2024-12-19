2e61610b7442d6c38b7431ea9204c8fdd510f7fd
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2024-12-19 19:13:03 +0000

    Set token refresh timestamp after user login

--- src/components/pages/auth/SignInForm.js
+++ src/components/pages/auth/SignInForm.js
@@ -16,6 +16,7 @@ import btnStyles from "../../../styles/Button.module.css";
 import appStyles from "../../../App.module.css";
 import { useSetCurrentUser } from "../../../contexts/CurrentUserContext";
 import { useRedirect } from "../../../hooks/useRedirect";
+import { setTokenTimestamp } from "../../../utils/utils";
 
 /**
  * SignInForm component renders a sign-in form for user authentication.
@@ -47,6 +48,7 @@ function SignInForm() {
         try {
             const {data} = await axios.post('/dj-rest-auth/login/', signInData);
             setCurrentUser(data.user);
+            setTokenTimestamp(data);
             history.goBack();
         } catch (err) {
             setErrors(err.response?.data);
