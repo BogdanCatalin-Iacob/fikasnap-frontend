f9414266ce5a4c8a2e5e3fd2f7a19d073b0aee27
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2024-12-19 19:58:10 +0000

    Add docstring

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import styles from '../styles/Avatar.module.css'
 
+/**
+ * Avatar component that displays an image and optional text.
+ *
+ * @param {Object} props - Component properties.
+ * @param {string} props.src - The source URL of the avatar image.
+ * @param {number} [props.height=45] - The height and width of the avatar image in pixels.
+ * @param {string} [props.text] - Optional text to display alongside the avatar image.
+ * @returns {JSX.Element} A span element containing an image and optional text.
+ */
 const Avatar = ({ src, height = 45, text }) => {
     return (
         <span>
