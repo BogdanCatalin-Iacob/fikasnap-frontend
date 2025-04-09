8f4f9a699e8611cf33d22ce4a4dbb449321306b3
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2025-04-09 18:41:11 +0100

    Fix typo

--- src/components/pages/profiles/Profile.js
+++ src/components/pages/profiles/Profile.js
@@ -37,7 +37,7 @@ const Profile = (props) => {
                     <Avatar src={image} height={imageSize} />
                 </Link>
             </div>
-            <div className={`mx-2 ${styles.WrodBreak}`}>
+            <div className={`mx-2 ${styles.WordBreak}`}>
                 <strong>{owner}</strong>
             </div>
             <div className={`text-right ${!mobile && 'ml-auto'}`}>
